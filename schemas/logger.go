4daf2408af5e4ef39b90d6cbe6c40a4d3cabd556
matvasenin <mat.vasenin@yandex.ru>
2026-01-15 12:23:46 +0300

    feat(schemas): implement human-readable LogLevels

---
+++ schemas/logger.go
@@ -0,0 +1,11 @@
+package schemas
+
+import "github.com/charmbracelet/log"
+
+var LogLevels = map[string]log.Level{
+	"debug": log.DebugLevel,
+	"info":  log.InfoLevel,
+	"warn":  log.WarnLevel,
+	"error": log.ErrorLevel,
+	"fatal": log.FatalLevel,
+}
