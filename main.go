e8df8f76ad915e3121d176024dd83adf4a93cc90
matvasenin <mat.vasenin@yandex.ru>
2026-01-16 00:29:34 +0300

    refactor(main): improve the code consistency

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"hexframe/snowfall/core"
 	"hexframe/snowfall/schemas"
 	"hexframe/snowfall/utils"
@@ -10,6 +11,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
 
@@ -19,7 +21,7 @@ import (
 
 type ConfigLog struct {
 	Mode     string `validate:"oneof=both stderr file"`
-	Filename string `validate:"required_unless=Mode stderr,filepath"`
+	Filename string `validate:"required_unless=Mode stderr"`
 	Level    string `validate:"oneof=fatal error warn info debug"`
 }
 type Config struct {
@@ -81,7 +83,7 @@ func main() {
 			os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o600,
 		)
 		if err != nil {
-			logger.Fatal("Could not open file for logging")
+			logger.Fatal("Could not create file for logging")
 		}
 		logWriters = append(logWriters, f)
 	}
@@ -94,14 +96,14 @@ func main() {
 	}
 
 	// Записываем значения из переменных окружения в структуру с настройками
-	logger.Info("Configuration: Processing environment variables...")
+	logger.Info("Config: Reading the environment variables...")
 	config.Host = utils.Getenv("SF_HOST", "127.0.0.1:8080")
 	config.InCheck = utils.Getenv("SF_IN_CHECK", "on")
 	config.InTransport = os.Getenv("SF_IN_TRANSPORT")
 	config.OutCheck = utils.Getenv("SF_OUT_CHECK", "on")
+	config.OutTransport = os.Getenv("SF_OUT_TRANSPORT")
 	config.OutCommand = strings.Split(os.Getenv("SF_OUT_CMD"), " ")
 	config.OutEndpoint = os.Getenv("SF_OUT_URL")
-	config.OutTransport = os.Getenv("SF_OUT_TRANSPORT")
 	config.Audit = utils.Getenv("SF_AUDIT", "off")
 	config.AuditEndpoint = os.Getenv("SF_AUDIT_URL")
 	config.AuditTreshold, err = strconv.Atoi(utils.Getenv("SF_AUDIT_TRESHOLD", "80"))
@@ -113,9 +115,10 @@ func main() {
 			"reason", "INCORRECT_NUMBER",
 		)
 	}
+	logger.Info("Config: Environment variables were loaded.")
 
 	// Проверяем переданные параметры на корректность
-	logger.Info("Configuration: Validating the provided parameters...")
+	logger.Info("Config: Validating the provided parameters...")
 	err = validate.Struct(config)
 	if err != nil {
 		validationErrors, exists := err.(validator.ValidationErrors)
@@ -128,45 +131,81 @@ func main() {
 			)
 		}
 	}
+	if config.InTransport == "stdio" && config.OutTransport == "stdio" {
+		logger.Fatal(
+			"Checkup failed:",
+			"reason", "MULTIPLE_STDIO",
+			"message", "Standard I/O can be used only on one side.",
+		)
+	}
+	logger.Info(
+		fmt.Sprintf("Config: All %d parameters are valid.", reflect.TypeFor[Config]().NumField()),
+	)
 
 	// В зависимости от транспортов могут быть использованы:
-	// - Прокси-сервер (HTTP -> HTTP)
-	// - HTTP-клиент (STDIO -> HTTP)
-	// - HTTP-сервер (HTTP -> STDIO)
-	// - Только Stdin и Stdout (STDIO -> STDIO)
-	if config.InTransport == "stdio" && config.OutTransport == "stdio" {
-		// TODO: Implement
-	} else {
+	// - Прокси-сервер (HTTP -> HTTP);
+	// - HTTP-клиент (STDIO -> HTTP);
+	// - HTTP-сервер (HTTP -> STDIO).
+	// Заметьте, STDIO -> STDIO быть не должно.
+	if config.InTransport == "http" && config.OutTransport == "http" {
+		logger.Info("Startup: Initilizing proxy server...")
 		serverURL, _ := url.Parse(config.OutEndpoint)
 		proxy := &httputil.ReverseProxy{}
+		// Настраиваем перехватчик сообщений вида "Запрос" (Request)
 		proxy.Director = func(req *http.Request) {
+			// Инициализруем проверку при определённых условиях:
+			// - Она включена для входа;
+			// - Тип HTTP-запроса - POST;
+			// - Запрос несёт полезную нагрузку - "Тело" (Body).
 			if config.InCheck == "on" && req.Method == http.MethodPost && req.Body != nil {
+				// Читаем тело в виде байтов
 				bodyBytes, _ := io.ReadAll(req.Body)
+				// Проверяем на сущестование извлечённых байтов
 				if len(bodyBytes) > 0 {
+					// Обрабатываем сообщение
 					bodyBytes, err = core.ProcessRequest(bodyBytes)
 					if err != nil {
+						// TODO: Внедрить обработку запроса при отрицательном результате
 						logger.Error(err)
 					}
 				}
+				// Возвращаем данные на место
 				req.Body = io.NopCloser(bytes.NewReader(bodyBytes))
 			}
 			req.URL = serverURL
 		}
+		// Настраиваем перехватчик сообщений вида "Ответ" (Response)
 		proxy.ModifyResponse = func(resp *http.Response) error {
+			// Инициализруем проверку при определённых условиях:
+			// - Она включена для выхода;
+			// - Тип HTTP-запроса - POST;
+			// - Ответ несёт полезную нагрузку - "Тело" (Body).
 			if config.OutCheck == "on" && resp.Request.Method == http.MethodPost && resp.Body != nil {
+				// Читаем тело в виде байтов
 				bodyBytes, _ := io.ReadAll(resp.Body)
+				// Проверяем на сущестование извлечённых байтов
 				if len(bodyBytes) > 0 {
+					// Обрабатываем сообщение
 					bodyBytes, err = core.ProcessResponse(bodyBytes)
 					if err != nil {
+						// TODO: Внедрить обработку ответа при отрицательном результате
 						logger.Error(err)
 					}
 				}
+				// Возвращаем данные на место
 				resp.Body = io.NopCloser(bytes.NewReader(bodyBytes))
 			}
 			return nil
 		}
-		logger.Fatal(
-			http.ListenAndServe(config.Host, proxy),
-		)
+		logger.Info("Startup: Server was initialized successfully.")
+		logger.Info("Startup: Launching the proxy server...")
+		err = http.ListenAndServe(config.Host, proxy)
+		if err != nil {
+			logger.Fatal("Proxy crashed:", "error", err)
+		}
+	} else if config.InTransport == "stdio" {
+		// TODO: Implement plain HTTP Server
+	} else if config.OutTransport == "stdio" {
+		// TODO: Implement plain HTTP Client
 	}
 }
