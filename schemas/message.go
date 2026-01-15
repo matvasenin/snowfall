f2992e1abb93551da850bc6cd6c53cfdd001c2c9
matvasenin <mat.vasenin@yandex.ru>
2026-01-15 12:23:11 +0300

    feat(schemas): implement MCP-dependent schemas

--- schemas/message.go
+++ schemas/message.go
@@ -1,8 +1,70 @@
 package schemas
 
-type Message struct {
-	JsonRPCVersion string `json:"jsonrpc"`
-	ID             uint32 `json:"id"`
-	Method         string `json:"method"`
-	Params         any    `json:"params"`
+type MCPClientInfo struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+type MCPServerInfo struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+type MCPClientCapabilities struct {
+	Elicitation any `json:"elicitation"`
+	Roots       any `json:"roots"`
+}
+type MCPServerCapabilities struct {
+	Logging any `json:"logging"`
+	Tools   any `json:"tools"`
+}
+type MCPToolInputProp struct {
+	Type        string `json:"type"`
+	Enum        []any  `json:"enum"`
+	Description string `json:"description"`
+	Default     any    `json:"default"`
+}
+type MCPToolInput struct {
+	Type       string                      `json:"type"`
+	Properties map[string]MCPToolInputProp `json:"properties"`
+	Required   []string                    `json:"required"`
+}
+type MCPTool struct {
+	Name        string       `json:"name"`
+	Title       string       `json:"title"`
+	Description string       `json:"description"`
+	InputSchema MCPToolInput `json:"inputSchema"`
+}
+type MCPResource struct{}
+
+type MCPRequestParams struct {
+	// Запрос может содержать огромный перечень различных параметров,
+	// что зависит от вызываемого метода (method: "...").
+	// Но самые важные для работы программы объявлены ниже.
+	ClientInfo      MCPClientInfo         `json:"clientInfo"`
+	ProtocolVersion string                `json:"protocolVersion" validate:"len=0|datetime=2006-01-02"`
+	Capabililties   MCPClientCapabilities `json:"capabilities"`
+}
+type MCPRequest struct {
+	JsonRPCVersion string           `json:"jsonrpc" validate:"eq=2.0"`
+	ID             uint32           `json:"id" validate:"number"`
+	Method         string           `json:"method" validate:"ascii"`
+	Params         MCPRequestParams `json:"params"`
+}
+type MCPResponseResult struct {
+	// Тип Content стоит расписать подробнее, включая type, text и др.
+	Content      []any                 `json:"content"`
+	Capabilities MCPServerCapabilities `json:"capabilities"`
+	ServerInfo   MCPServerInfo         `json:"serverInfo"`
+	Tools        []MCPTool             `json:"tools"`
+	Resources    []MCPResource         `json:"resources"`
+}
+type MCPResponseError struct {
+	Code    int16  `json:"code"`
+	Message string `json:"message"`
+}
+type MCPResponse struct {
+	JsonRPCVersion  string            `json:"jsonrpc" validate:"eq=2.0"`
+	ID              uint32            `json:"id" validate:"number"`
+	Result          MCPResponseResult `json:"result"`
+	ProtocolVersion string            `json:"protocolVersion" validate:"len=0|datetime=2006-01-02"`
+	Error           MCPResponseError  `json:"error"`
 }
